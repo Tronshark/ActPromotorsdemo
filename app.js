ef514a9d0408c7e923301ceccad21ae3e0674489
Tronshark <varma@maantt.com>
2024-05-06 10:57:28 +0530

    first commit

--- app.js
+++ app.js
@@ -9,38 +9,33 @@ angular.module('app', ['ui.router']).config([
         templateUrl: 'home.html'
       })
       .state('Projects', {
-        url: '/Masterpage-template-master/Projects.html',
+        url: '/Projects.html',
         templateUrl: 'Projects.html'
       })
 
       .state('Contact', {
-        url: '/Masterpage-template-master/Contact.html',
+        url: '/Contact.html',
         templateUrl: 'Contact.html'
       })
       .state('site1', {
-        url: '/Masterpage-template-master/assets/Projectdetails/Site-1/site1.html',
-        templateUrl:
-          '/Masterpage-template-master/assets/Projectdetails/Site-1/site1.html'
+        url: '/assets/Projectdetails/Site-1/site1.html',
+        templateUrl: '/assets/Projectdetails/Site-1/site1.html'
       })
       .state('site2', {
-        url: '/Masterpage-template-master/assets/Projectdetails/Site-2/site2.html',
-        templateUrl:
-          '/Masterpage-template-master/assets/Projectdetails/Site-2/site2.html'
+        url: '/assets/Projectdetails/Site-2/site2.html',
+        templateUrl: '/assets/Projectdetails/Site-2/site2.html'
       })
       .state('site3', {
-        url: '/Masterpage-template-master/assets/Projectdetails/Site-3/site3.html',
-        templateUrl:
-          '/Masterpage-template-master/assets/Projectdetails/Site-3/site3.html'
+        url: '/assets/Projectdetails/Site-3/site3.html',
+        templateUrl: '/assets/Projectdetails/Site-3/site3.html'
       })
       .state('site4', {
-        url: '/Masterpage-template-master/assets/Projectdetails/Site-4/site4.html',
-        templateUrl:
-          '/Masterpage-template-master/assets/Projectdetails/Site-4/site4.html'
+        url: '/assets/Projectdetails/Site-4/site4.html',
+        templateUrl: '/assets/Projectdetails/Site-4/site4.html'
       })
       .state('site5', {
-        url: '/Masterpage-template-master/assets/Projectdetails/Site-5/site5.html',
-        templateUrl:
-          '/Masterpage-template-master/assets/Projectdetails/Site-5/site5.html'
+        url: '/assets/Projectdetails/Site-5/site5.html',
+        templateUrl: '/assets/Projectdetails/Site-5/site5.html'
       })
   }
 ])
