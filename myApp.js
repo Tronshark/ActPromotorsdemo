e42fb1a977d0c869540a268057f09ff5e0c13bd3
Ravivarma <104569962+Tronshark@users.noreply.github.com>
2024-05-15 11:24:35 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -6,36 +6,36 @@ var myApp = angular.module('app', ['ui.router']).config([
     $stateProvider
       .state('Home', {
         url: '/',
-        templateUrl: '/src/home.html'
+        templateUrl: 'src/home.html'
       })
       .state('Projects', {
-        url: '/src/Projects.html',
-        templateUrl: '/src/Projects.html'
+        url: 'src/Projects.html',
+        templateUrl: 'src/Projects.html'
       })
 
       .state('Contact', {
-        url: '/Contact.html',
-        templateUrl: '/src/Contact.html'
+        url: 'Contact.html',
+        templateUrl: 'src/Contact.html'
       })
       .state('site1', {
-        url: '/assets/Projectdetails/Site-1/site1.html',
-        templateUrl: '/assets/Projectdetails/Site-1/site1.html'
+        url: 'assets/Projectdetails/Site-1/site1.html',
+        templateUrl: 'assets/Projectdetails/Site-1/site1.html'
       })
       .state('site2', {
-        url: '/assets/Projectdetails/Site-2/site2.html',
-        templateUrl: '/assets/Projectdetails/Site-2/site2.html'
+        url: 'assets/Projectdetails/Site-2/site2.html',
+        templateUrl: 'assets/Projectdetails/Site-2/site2.html'
       })
       .state('site3', {
-        url: '/assets/Projectdetails/Site-3/site3.html',
-        templateUrl: '/assets/Projectdetails/Site-3/site3.html'
+        url: 'assets/Projectdetails/Site-3/site3.html',
+        templateUrl: 'assets/Projectdetails/Site-3/site3.html'
       })
       .state('site4', {
-        url: '/assets/Projectdetails/Site-4/site4.html',
-        templateUrl: '/assets/Projectdetails/Site-4/site4.html'
+        url: 'assets/Projectdetails/Site-4/site4.html',
+        templateUrl: 'assets/Projectdetails/Site-4/site4.html'
       })
       .state('site5', {
-        url: '/assets/Projectdetails/Site-5/site5.html',
-        templateUrl: '/assets/Projectdetails/Site-5/site5.html'
+        url: 'assets/Projectdetails/Site-5/site5.html',
+        templateUrl: 'assets/Projectdetails/Site-5/site5.html'
       })
   }
 ])
