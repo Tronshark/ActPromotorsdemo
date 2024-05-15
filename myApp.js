fa354c332e93165abcf5366a63078d232ac09d7d
Ravivarma <104569962+Tronshark@users.noreply.github.com>
2024-05-15 12:21:22 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -18,23 +18,23 @@ var myApp = angular.module('app', ['ui.router']).config([
         templateUrl: 'src/Contact.html'
       })
       .state('site1', {
-        url: 'assets/Projectdetails/Site-1/site1.html',
+        url: '/assets/Projectdetails/Site-1/site1.html',
         templateUrl: 'assets/Projectdetails/Site-1/site1.html'
       })
       .state('site2', {
-        url: 'assets/Projectdetails/Site-2/site2.html',
+        url: '/assets/Projectdetails/Site-2/site2.html',
         templateUrl: 'assets/Projectdetails/Site-2/site2.html'
       })
       .state('site3', {
-        url: 'assets/Projectdetails/Site-3/site3.html',
+        url: '/assets/Projectdetails/Site-3/site3.html',
         templateUrl: 'assets/Projectdetails/Site-3/site3.html'
       })
       .state('site4', {
-        url: 'assets/Projectdetails/Site-4/site4.html',
+        url: '/assets/Projectdetails/Site-4/site4.html',
         templateUrl: 'assets/Projectdetails/Site-4/site4.html'
       })
       .state('site5', {
-        url: 'assets/Projectdetails/Site-5/site5.html',
+        url: '/assets/Projectdetails/Site-5/site5.html',
         templateUrl: 'assets/Projectdetails/Site-5/site5.html'
       })
   }
